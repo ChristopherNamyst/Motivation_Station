2f922b9b2181a4a927ce487127a0d683c9c03403
Christopher Paul Namyst <cpnamyst79@gmail.com>
2018-01-03 14:49:00 -0500

    fixed firebase issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBcsdZbMgJ1bvs8msTINyYpQWRbjHvhC5c",
-    authDomain: "motivationstation-36fee.firebaseapp.com",
-    databaseURL: "https://motivationstation-36fee.firebaseio.com",
-    projectId: "motivationstation-36fee",
-    storageBucket: "motivationstation-36fee.appspot.com",
-    messagingSenderId: "713163355180"
+    apiKey: "AIzaSyA7ISL4pAadgOSKnllNKAzp8JARwM2Opw8",
+    authDomain: "motivationstation-5e322.firebaseapp.com",
+    databaseURL: "https://motivationstation-5e322.firebaseio.com",
+    projectId: "motivationstation-5e322",
+    storageBucket: "",
+    messagingSenderId: "127232835752"
   };
 
   export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 
+
+
+
+
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log('user has signed in or up', user);
+
+  } else {
+    console.log('user has signed out or still needs to sign in')
+  }
+})
+
+
 ReactDOM.render(
   <Router path="/" history={browserHistory}>
           <Route path="/app" component={App} />
